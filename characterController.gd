268c0cd311606bf521e88695377a2ab342e91d3d
ShaneP-dash <shp4349@g.rit.edu>
2024-10-04 18:04:48 -0400

    Created walls and player object
    Created wall prefab with collision and created a player prefab with movement and collision

---
+++ characterController.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(_delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var xDirection := Input.get_axis("ui_left", "ui_right")
+	var yDirection := Input.get_axis("ui_up", "ui_down")
+	if xDirection:
+		velocity.x = xDirection * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if yDirection:
+		velocity.y = yDirection * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
