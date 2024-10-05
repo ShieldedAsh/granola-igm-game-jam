5d3c1ff863dc19c78af433fb3f598fe69cd4b1cf
ShaneP-dash <shp4349@g.rit.edu>
2024-10-04 21:28:14 -0400

    Finished Raycasting with rotation

--- scripts/parentAI.gd
+++ scripts/parentAI.gd
@@ -7,7 +7,7 @@ func _physics_process(delta: float) -> void:
 	var space_state = get_world_2d().direct_space_state
 	# use global coordinates, not local to node
 	for i in range(3,23,2):
-		var angle = ((7.5 * i * PI) - (rotation * PI))/ 180
+		var angle = (7.5 * i * PI) - rotation
 		var query = PhysicsRayQueryParameters2D.create(global_position, Vector2(100 * cos(angle)+position.x, -100 * sin(angle)+position.y))
 		var result = space_state.intersect_ray(query)
 		if result:
