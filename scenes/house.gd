d7ce8b0d9cf7acc57b495ae993a154ff5943c55d
TheGreenBandana <119015455+TheGreenBandana@users.noreply.github.com>
2024-10-05 22:56:04 -0400

    made it so you can win, changed player sprite

--- scenes/house.gd
+++ scenes/house.gd
@@ -6,8 +6,9 @@ signal playerLoss
 func _ready():
 	$AudioStreamPlayer2D.play()
 
-func _on_player_victory():
-	playerVictory.emit()
-
 func _on_player_loss():
 	playerLoss.emit()
+
+func _on_area_2d_body_entered(body):
+	if body is CharacterBody2D:
+		playerVictory.emit()
