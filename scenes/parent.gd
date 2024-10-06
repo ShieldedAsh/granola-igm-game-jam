15a756d881069d14ca33516e4a6cb1f2c215e7bc
ShaneP-dash <shp4349@g.rit.edu>
2024-10-05 20:27:42 -0400

    navigation shit

--- scenes/parent.gd
+++ scenes/parent.gd
@@ -2,47 +2,38 @@ extends CharacterBody2D
 
 @onready var raycastFolder = $Raycasts
 
-var movement_speed: float = 200.0
-var movement_target_position: Vector2 = Vector2(60.0,180.0)
+var speed = 300
+var accel = 7
+var physicsParameters = PhysicsPointQueryParameters2D.new()
+var patrolStops = [Vector2(81,61),Vector2(81,569),Vector2(974,565),Vector2(974,112),Vector2(584,106),Vector2(578,349)]
+var currentStop = 4
+var stopDirection = 1
 
-var playerInRectangle:bool = false
-@export var playerNode: CharacterBody2D
-
-@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
-
-func _ready():
-	# These values need to be adjusted for the actor's speed
-	# and the navigation layout.
-	navigation_agent.path_desired_distance = 4.0
-	navigation_agent.target_desired_distance = 4.0
-
-	# Make sure to not await during _ready.
-	actor_setup.call_deferred()
-	
-
-func actor_setup():
-	# Wait for the first physics frame so the NavigationServer can sync.
-	await get_tree().physics_frame
-
-	# Now that the navigation map is no longer empty, set the movement target.
-	set_movement_target(movement_target_position)
-
-func set_movement_target(movement_target: Vector2):
-	navigation_agent.target_position = movement_target
+@onready var nav: NavigationAgent2D = $NavigationAgent2D
 
 func _process(_delta):
 	var space_state = get_world_2d().direct_space_state
-	print("process")
 	for i:RayCast2D in raycastFolder.get_children():
 		if i.is_colliding() and i.get_collider() is CharacterBody2D:
 			print("raycast hit a characterbody")
 			
+func _physics_process(delta):
+	physicsParameters.collide_with_areas = true 
+	physicsParameters.position = get_global_mouse_position()
 	
-	if navigation_agent.is_navigation_finished():
-		return
+	# Add the gravity
+	var direction = Vector2()
+	if get_world_2d().direct_space_state.intersect_point(physicsParameters, 1):
+		print("HIT")
+		if (currentStop == 0 || currentStop == 5):
+			stopDirection *= -1
+		currentStop += stopDirection
 
-	var current_agent_position: Vector2 = global_position
-	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
-
-	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	nav.target_position = patrolStops[currentStop]
+	
+	direction = nav.get_next_path_position() - global_position
+	direction = direction.normalized()
+	
+	velocity = velocity.lerp(direction * speed, accel * delta)
+	
 	move_and_slide()
